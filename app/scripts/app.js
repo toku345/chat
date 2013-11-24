18aa9bd67d53c0f543a2c2196b6b697fd30e037f
SatoShun <shun.sato1@gmail.com>
2013-11-24 12:58:10 +0900

    base scss, javascript

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,9 @@
 var App = angular.module('app', [
-  'ngRoute',
+  'ngRoute'
 ])
-// .config(function($interpolateProvider) {
-//   $interpolateProvider.startSymbol('((');
-//   $interpolateProvider.endSymbol('))');
-// })
+.config(['$interpolateProvider', function($interpolateProvider) {
+  $interpolateProvider.startSymbol('((');
+  $interpolateProvider.endSymbol('))');
+}])
 .controller('MainCtrl', ['$scope', function($scope) {
-  
 }]);
--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,5 +1,9 @@
-var App = angular.module('app', ['ngRoute']).controller('MainCtrl', [
-    '$scope',
-    function ($scope) {
-    }
-  ]);
+var App = angular.module('app', [
+  'ngRoute'
+])
+.config(['$interpolateProvider', function($interpolateProvider) {
+  $interpolateProvider.startSymbol('((');
+  $interpolateProvider.endSymbol('))');
+}])
+.controller('MainCtrl', ['$scope', function($scope) {
+}]);
