ae902520e4b09329712efbe949ad973a70f962cb
Fumitaka Tokumitsu <toku345@gmail.com>
2013-12-08 12:04:44 +0900

    output 'MongoDB connected' log

--- models/index.js
+++ models/index.js
@@ -3,5 +3,9 @@ var db = require('mongoose'),
   dbName = 'chat';
 
 db.connect('mongodb://localhost/' + dbName);
+db.connection.on('connected', function() {
+  console.log('MongoDB connected');
+});
+
 
 module.exports = db;
